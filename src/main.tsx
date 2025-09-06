8a03dea3aa3bf883ac3db5a7559fd0045160347b
Lennonpdc <lennonpaulcabrera@gmail.com>
2025-09-07 01:02:30 +0800

    refactor: servives folder link

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 // Import Ant Design styles
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/services/:id" />
+      </Routes>
+    
+    </BrowserRouter>
+   
   </StrictMode>,
 )
